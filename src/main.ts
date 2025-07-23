d342ed026d0b6920c7eac8707a19b0d5e0b822db
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-07-22 18:35:43 -0700

    Add pixi.js with a mock function.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Quaboom!</h1>
-  </div>
-`;
+import { Application, Graphics } from "pixi.js";
+
+(async () => {
+  const app = new Application();
+
+  // Initialize the application
+  await app.init({ background: "#000", width: 500, height: 700 });
+
+  // Append the application canvas to the document body
+  document.body.appendChild(app.canvas);
+
+  const graphics = new Graphics().rect(100, 100, 100, 100).fill(0xff0000);
+  graphics.pivot.set(150, 150);
+  app.stage.addChild(graphics);
+
+  app.ticker.add((time) => {
+    graphics.rotation += time.deltaMS / 1000;
+  });
+})();
