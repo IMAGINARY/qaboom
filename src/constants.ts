9cad103cf9dcfa74af3773ab4a1c0a7d9a4e9cdf
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-08-07 03:43:10 -0700

    Adjust screen size.

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const WIDTH = 1200;
-export const HEIGHT = 850;
-export const CELL_SIZE = 60;
+export const HEIGHT = 750;
+export const CELL_SIZE = 55;
 export const PIECE_RADIUS = (CELL_SIZE / 2) * 0.875;
 
 export const BOARD_WIDTH = 6;
