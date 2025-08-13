bc76e286307ec77da797803fe3f386ab8d1eb7cc
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-08-13 04:22:22 -0700

    Add more levels to the campaign.

--- src/levels.ts
+++ src/levels.ts
@@ -3,7 +3,18 @@ import QubitPair from "./QubitPair";
 import GatePiece from "./GatePiece";
 import MeasurementPiece from "./MeasurementPiece";
 import { choice, shuffle } from "./random";
-import { PLUS, MINUS, ONE, ZERO, PLUS_I, MINUS_I } from "./quantum";
+import {
+  PLUS,
+  MINUS,
+  ONE,
+  ZERO,
+  PLUS_I,
+  MINUS_I,
+  applyGate,
+  rotateXGate,
+  rotateYGate,
+  rotateZGate,
+} from "./quantum";
 import type { Piece } from "./Deck";
 
 export interface Level {
@@ -24,6 +35,94 @@ export const freeMode: Level = {
   },
 };
 
+export const secondaryColors: Level[] = [
+  {
+    deal: () => {
+      const qubits = range(0, 8).map((theta) => {
+        return applyGate(rotateXGate((theta * Math.PI) / 4), ZERO);
+      });
+      const random = () => choice(qubits);
+      let buffer = [];
+      for (let _i of range(5)) {
+        buffer.push(new QubitPair(random(), random()));
+      }
+      buffer.push(new GatePiece("X", Math.PI));
+      buffer.push(new MeasurementPiece(random()));
+      return buffer;
+    },
+  },
+  {
+    deal: () => {
+      const qubits = range(0, 8).map((theta) => {
+        return applyGate(rotateYGate((theta * Math.PI) / 8), ZERO);
+      });
+      const random = () => choice(qubits);
+      let buffer = [];
+      for (let _i of range(5)) {
+        buffer.push(new QubitPair(random(), random()));
+      }
+      buffer.push(new GatePiece("Y", Math.PI));
+      buffer.push(new MeasurementPiece(random()));
+      return buffer;
+    },
+  },
+  {
+    deal: () => {
+      const qubits = range(0, 8).map((theta) => {
+        return applyGate(rotateZGate((theta * Math.PI) / 4), PLUS);
+      });
+      const random = () => choice(qubits);
+      let buffer = [];
+      for (let _i of range(5)) {
+        buffer.push(new QubitPair(random(), random()));
+      }
+      buffer.push(new GatePiece("Z", Math.PI));
+      buffer.push(new MeasurementPiece(random()));
+      return buffer;
+    },
+  },
+  {
+    deal: () => {
+      const qubits = [
+        ZERO,
+        ONE,
+        PLUS,
+        MINUS,
+        PLUS_I,
+        MINUS_I,
+        ...range(0, 4).map((theta) => {
+          return applyGate(
+            rotateXGate((theta * Math.PI) / 2 + Math.PI / 4),
+            ZERO
+          );
+        }),
+        ...range(0, 4).map((theta) => {
+          return applyGate(
+            rotateYGate((theta * Math.PI) / 2 + Math.PI / 4),
+            ZERO
+          );
+        }),
+        ...range(0, 4).map((theta) => {
+          return applyGate(
+            rotateZGate((theta * Math.PI) / 2 + Math.PI / 4),
+            PLUS
+          );
+        }),
+      ];
+      const random = () => choice(qubits);
+      let buffer = [];
+      for (let _i of range(5)) {
+        buffer.push(new QubitPair(random(), random()));
+      }
+      buffer.push(new GatePiece(choice(["X", "Y", "Z"]), Math.PI));
+      buffer.push(new MeasurementPiece(random()));
+      return buffer;
+    },
+  },
+];
+
+// export const campaign = [freeMode];
+// export const campaign = secondaryColors;
 export const campaign: Level[] = [
   {
     deal: () => {
@@ -84,5 +183,6 @@ export const campaign: Level[] = [
       return buffer;
     },
   },
+  ...secondaryColors,
   freeMode,
 ];
