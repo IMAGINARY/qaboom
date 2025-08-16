216225e6e1d605e8f0dd3b5ce756e5684211f479
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-08-15 20:16:01 -0700

    Add arrow to measurement piece design.

--- src/nodes/MeasurementPiece.ts
+++ src/nodes/MeasurementPiece.ts
@@ -7,6 +7,7 @@ import GameNode from "./GameNode";
 // Represents a measurement along an axis
 export default class MeasurementPiece extends GameNode {
   sprite: Graphics;
+  rod: Graphics;
   base: Qubit;
   ortho: Qubit;
   constructor(base: Qubit) {
@@ -14,20 +15,31 @@ export default class MeasurementPiece extends GameNode {
     this.base = base;
     this.ortho = getOrtho(base);
     this.sprite = new Graphics();
+    this.rod = new Graphics();
     this.view.addChild(this.sprite);
+    this.view.addChild(this.rod);
     this.draw();
   }
 
   draw() {
-    const baseColor = getColor(getBlochCoords(this.base));
+    const coords = getBlochCoords(this.base);
+    const baseColor = getColor(coords);
+    // const secondaryColor = getSecondaryColor(coords);
     const orthoColor = getColor(getBlochCoords(this.ortho));
     this.sprite
       .clear()
       .circle(0, 0, PIECE_RADIUS)
-      .stroke({ color: "grey", width: 2 })
+      .stroke({ color: orthoColor, width: 2 })
       .fill(baseColor)
-      .circle(0, 0, PIECE_RADIUS / 2)
-      .fill(orthoColor);
+      .circle(0, 0, PIECE_RADIUS / 3)
+      .stroke({ color: orthoColor, width: 6 });
+    this.rod
+      .clear()
+      .moveTo(0, 0)
+      .lineTo(0, PIECE_RADIUS)
+      .stroke({ color: orthoColor, width: 4, cap: "round" });
+    this.rod.rotation = coords.phi;
+    this.rod.scale.y = Math.sin(coords.theta);
   }
 
   flip() {
