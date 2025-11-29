fb4a6c30f59add41d7f2c955eff39d8ad84adf64
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-11-29 01:22:01 -0800

    Gamepad input.

--- src/inputs.ts
+++ src/inputs.ts
@@ -1,11 +1,4 @@
-export interface PlayerInput {
-  left: string;
-  right: string;
-  up: string;
-  down: string;
-  flip: string;
-  hold: string;
-}
+import { difference } from "lodash-es";
 
 export type Input = string;
 type Callback = (input: Input) => void;
@@ -13,6 +6,10 @@ type Callback = (input: Input) => void;
 class InputManager {
   keydownCallbacks: Callback[] = [];
   keyupCallbacks: Callback[] = [];
+
+  gamepadPressed: Input[] = [];
+  // simulate behavior of keypresses
+  gamepadCurrent?: Input = undefined;
 
   constructor() {
     document.addEventListener("keydown", (e) => {
@@ -25,6 +22,66 @@ class InputManager {
         cb(keyInputs[e.key]);
       }
     });
+  }
+
+  tick() {
+    const gamepads = navigator.getGamepads();
+    let newInputs: Input[] = [];
+    if (gamepads[0]) {
+      newInputs = newInputs.concat(
+        this.getGamepadButtons("player1", gamepads[0])
+      );
+    }
+    if (gamepads[1]) {
+      newInputs = newInputs.concat(
+        this.getGamepadButtons("player2", gamepads[1])
+      );
+    }
+    for (let input of difference(this.gamepadPressed, newInputs)) {
+      if (this.gamepadCurrent === input) {
+        this.gamepadCurrent = undefined;
+      }
+      for (let cb of this.keyupCallbacks) {
+        cb(input);
+      }
+    }
+    for (let input of difference(newInputs, this.gamepadPressed)) {
+      this.gamepadCurrent = input;
+      for (let cb of this.keydownCallbacks) {
+        cb(input);
+      }
+    }
+    // TODO repeat inputs
+    // if (this.gamepadCurrent) {
+    //   for (let cb of this.keydownCallbacks) {
+    //     cb(this.gamepadCurrent);
+    //   }
+    // }
+
+    this.gamepadPressed = newInputs;
+  }
+
+  getGamepadButtons(playerIndex: string, gp: Gamepad) {
+    const buttons = [];
+    if (gp.axes[0] < -0.5 || gp.buttons[14].pressed) {
+      buttons.push(`${playerIndex}.left`);
+    }
+    if (gp.axes[0] > 0.5 || gp.buttons[15].pressed) {
+      buttons.push(`${playerIndex}.right`);
+    }
+    if (gp.axes[1] < -0.5 || gp.buttons[12].pressed) {
+      buttons.push(`${playerIndex}.up`);
+    }
+    if (gp.axes[1] > 0.5 || gp.buttons[13].pressed) {
+      buttons.push(`${playerIndex}.down`);
+    }
+    if (gp.buttons[0].pressed) {
+      buttons.push(`${playerIndex}.flip`);
+    }
+    if (gp.buttons[1].pressed) {
+      buttons.push(`${playerIndex}.hold`);
+    }
+    return buttons;
   }
 
   addKeydownListener(cb: Callback) {
