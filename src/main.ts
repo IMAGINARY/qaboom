465e8d91449d034d699170be378f51c3a1985075
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-11-28 20:28:06 -0800

    Add German language

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { loadLanguages } from "./i18n";
 (async () => {
   const urlParams = new URLSearchParams(window.location.search);
   await loadLanguages(["en"], urlParams.get("lang") || "en");
-  // load "fake" language for testing translations
-  // await loadLanguages(["en", "fake"], urlParams.get("lang") || "en");
+  // load more languages for testing translations
+  // await loadLanguages(["en", "de"], urlParams.get("lang") || "en");
   const game = new Game();
   await game.start();
 })();
