cdb672a8ebebf2c1d56d4b85d38e89d7ed2aabc0
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-11-28 23:49:36 -0800

    input manager class

--- src/inputs.ts
+++ src/inputs.ts
@@ -6,6 +6,71 @@ export interface PlayerInput {
   flip: string;
   hold: string;
 }
+
+type Callback = (input: string) => void;
+
+class InputManager {
+  keydownCallbacks: Callback[] = [];
+  keyupCallbacks: Callback[] = [];
+
+  constructor() {
+    document.addEventListener("keydown", (e) => {
+      for (let cb of this.keydownCallbacks) {
+        cb(keyInputs[e.key]);
+      }
+    });
+    document.addEventListener("keyup", (e) => {
+      for (let cb of this.keyupCallbacks) {
+        cb(keyInputs[e.key]);
+      }
+    });
+  }
+
+  addKeydownListener(cb: Callback) {
+    this.keydownCallbacks.push(cb);
+  }
+
+  removeKeydownListener(cb: Callback) {
+    const index = this.keydownCallbacks.indexOf(cb);
+    if (index >= 0) {
+      this.keydownCallbacks.splice(index, 1);
+    }
+  }
+
+  addKeyupListener(cb: Callback) {
+    this.keyupCallbacks.push(cb);
+  }
+
+  removeKeyupListener(cb: Callback) {
+    const index = this.keydownCallbacks.indexOf(cb);
+    if (index >= 0) {
+      this.keyupCallbacks.splice(index, 1);
+    }
+  }
+
+  // TODO add gamepads
+}
+
+export const inputManager = new InputManager();
+
+const keyInputs: Record<string, string> = {
+  r: "refresh",
+  p: "pause",
+  t: "translate",
+  a: "player1.left",
+  d: "player1.right",
+  w: "player1.up",
+  s: "player1.down",
+  e: "player1.flip",
+  q: "player1.hold",
+  j: "player2.left",
+  l: "player2.right",
+  i: "player2.up",
+  k: "player2.down",
+  o: "player2.flip",
+  u: "player2.hold",
+};
+
 export const inputs = {
   refresh: "r",
   pause: "p",
